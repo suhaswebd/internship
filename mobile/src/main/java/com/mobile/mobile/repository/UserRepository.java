8d3762b709380eac805eb64d47485a600e6981f2
suhaswebd <suhascr777@gmail.com>
2023-11-21 21:17:29 +0530

    first commit

--- mobile/src/main/java/com/mobile/mobile/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mobile.mobile.repository;
-
-import com.mobile.mobile.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
